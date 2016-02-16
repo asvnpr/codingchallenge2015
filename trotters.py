3d8d69404d8286a7835e58ef315f44c33dfc73b7
Alejandro Vega <asvnpr@users.noreply.github.com>
2016-02-16 15:24:02 -0500

    Update trotters.py
    added cases for 10-20 million, 10-20 thousand. handles combinations of *teens. added some whitespace for readability. added a few comments

--- trotters.py
+++ trotters.py
@@ -21,45 +21,61 @@ def intToWord(n):
             digits.append(int(numStr[i]))
 
         # go through list and convert digits to words and build the string
-        teen = False
+        teen = False #used for preventing
         while (l > 0):
             #10^8 to 10^6
             if (l == 9):
-                if (digits[-l] != 0 or digits[-(l - 1)] != 0):
+                if (digits[-(l - 1)] != 0 or digits[-(l - 2)] != 0):
+                    print l-1
                     build.append(ones[digits[-l]]), build.append(others[0]), build.append("and")
                 else:
                     build.append(ones[digits[-l]]), build.append(others[0]), build.append(others[2])
-            elif (l == 8 and digits[-l] != 0):
-                if (digits[-(l - 1)] != 0):
-                    build.append(tens[digits[-l]])
-                else:
-                    build.append(tens[digits[-l]]), build.append(others[2])
-            elif (l == 7 and digits[-l] != 0):
-                    build.append(ones[digits[-l]]), build.append(others[2])
+            elif (l == 8 and digits[-8] == 1 and digits[-7] > 0): #10^4 is < 20,000,000 and > 10,000,000
+                tmp = digits[-7] + 10
+                build.append(teens[tmp]), build.append(others[2])
+                teen = True
+            elif (teen != True and (l == 8 or l == 7)): #10^4 is not < 20,000,000 and > 10,000,000
+                if (l == 8 and digits[-l] != 0):
+                    if (digits[-(l - 1)] != 0):
+                        build.append(tens[digits[-l]])
+                    else:
+                        build.append(tens[digits[-l]]), build.append(others[2])
+                elif (l == 7 and digits[-l] != 0):
+                        build.append(ones[digits[-l]]), build.append(others[2])
             #10^5 to 10^3
             elif (l == 6 and digits[-l] != 0):
                 if (digits[-(l - 1)] != 0 or digits[-(l - 2)] != 0):
                     build.append(ones[digits[-l]]), build.append(others[0]), build.append("and")
                 else:
                     build.append(ones[digits[-l]]), build.append(others[0]), build.append(others[1])
-            elif (l == 5 and digits[-l] != 0):
-                if (digits[-(l - 1)] != 0):
-                    build.append(tens[digits[-l]])
-                else:
-                    build.append(tens[digits[-l]]), build.append(others[1])
-            elif (l == 4 and digits[-l] != 0):
-                    build.append(ones[digits[-l]]), build.append(others[1])
+
+            elif (l == 5 and digits[-5] == 1 and digits[-4] > 0): #10^4 is < 20,000 and > 10,000
+                tmp = digits[-4] + 10
+                build.append(teens[tmp]), build.append(others[1])
+                teen = True
+
+            elif (teen != True and (l == 5 or l == 4)): #10^4 is not < 20,000 and > 10,000
+                if (l == 5 and digits[-l] != 0):
+                    if (digits[-(l - 1)] != 0):
+                        build.append(tens[digits[-l]])
+                    else:
+                        build.append(tens[digits[-l]]), build.append(others[1])
+                elif (l == 4 and digits[-l] != 0):
+                        build.append(ones[digits[-l]]), build.append(others[1])
+
             #10^2 to 10^0
             elif (l == 3 and digits[-l] != 0):
                 if (digits[-(l - 1)] != 0 or digits[-(l - 2)] != 0):
                     build.append(ones[digits[-l]]), build.append(others[0]), build.append("and")
                 else:
                     build.append(ones[digits[-l]]), build.append(others[0])
-            elif (l == 2 and digits[-2] == 1 and digits[-1] > 0 and teen == False):
-                tmp = digits[len(digits) - 1] + 10
+
+            elif (l == 2 and digits[-2] == 1 and digits[-1] > 0): #cases where 10^1 is < 20 and > 10
+                tmp = digits[-1] + 10
                 build.append(teens[tmp])
                 teen = True
-            elif (teen != True):
+
+            elif (teen != True and (l == 2 or l == 1)): #10^1 is not < 20 and > 10
                 if (l == 2 and digits[-l] != 0):
                     if (digits[-(l - 1)] != 0):
                         build.append(tens[digits[-l]])
@@ -68,7 +84,8 @@ def intToWord(n):
                 elif (l == 1 and digits[-l] != 0):
                     build.append(ones[digits[-l]])
             l -= 1
-        word = ' '.join(build)
+        teen = False #reset teen bool in cases where n is of form #15,#15,#15 or similar
+        word = ' '.join(build) #add spaces between words and save the completed string
         return word
 
 n = int(raw_input("Enter a whole number between 0 and 999,999,999: "))
@@ -76,13 +93,3 @@ while (n > 999999999 or n < 0):
     print "Error! You entered a value that was too large or too small."
     n = int(raw_input("Please enter a whole number between 0 and 999,999,999: "))
 print intToWord(n)
-
-
-
-
-
-
-
-
-
-
